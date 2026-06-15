91a94bb9e7806b07d6683c3c1b2224f4bcc588cb
宮澤昭海 <>
2026-06-16 08:49:08 +0900

    api呼び出しまで作成

---
+++ frontend/src/app/providers.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import React, { useState } from "react"
+
+type ProvidersProps = { children: React.ReactNode }
+
+export default function Providers({ children }: ProvidersProps) {
+    const [queryClient] = useState(() => new QueryClient())
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
+
+
+
